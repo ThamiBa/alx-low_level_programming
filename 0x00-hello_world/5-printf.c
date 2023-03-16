406e3b40573f10c3850b630e8ca34f3942ce4edf
thamibaladi <baladithami@gmail.com>
2023-03-16 16:47:20 -0700

    Hello,printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,8 +5,9 @@
  *
  * Return: 0 when successful
  */
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
