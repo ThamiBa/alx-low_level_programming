14dfaca1b364c4b47f3f2871aaba8c22908a457c
Anouarbelouafi <125509083+Anouarbelouafi@users.noreply.github.com>
2023-03-17 20:50:58 +0100

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,13 +7,13 @@
  */
 int main(void)
 {
-    int i;
+	int i;
 
-    for (i = 97 ; i <= 122 ; i++)
-    {
-        putchar(i);
-    }
-    putchar(10);
-    return (0);
+	for (i = 97 ; i <= 122 ; i++)
+	{
+		putchar(i);
+	}
+	putchar(10);
+	return (0);
 
 }
