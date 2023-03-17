a14ff6f0efb9765d9f76c1e7a928f669afd7695d
thamibaladi <baladithami@gmail.com>
2023-03-16 18:55:42 -0700

    Unix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ * main - and that piece of art is useful\" - Dora Korpar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char qte[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(STDERR_FILENO, qte, sizeof(qte) - 1);
+return (1);
+}
